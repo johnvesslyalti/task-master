c4b6e76816ceb43d28e267b9783e101cbe5cc3b1
johnvesslyalti <altijohnvessly@gmail.com>
2025-04-30 07:18:07 +0530

    refactor: clean up imports in AddTask component and update API base URL in api utility

--- frontend/src/pages/AddTask.jsx
+++ frontend/src/pages/AddTask.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from "react"
+import { useContext, useState } from "react"
 import { AuthContext } from "../context/AuthContext";
 import { GoArrowLeft } from "react-icons/go";
 import { useNavigate, useParams } from "react-router-dom";
--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://task-master-lxkh.onrender.com",
+    baseURL: "https://task-master-lxkh.onrender.com/api",
 });
 
 export default api;
